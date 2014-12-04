6301e2be36867c6531b94f81a84384dd70e09a7d
iisjreg-dev <iisjreg@googlemail.com>
2014-12-04 14:10:51 +0000

    added n-player scoring!

--- bower_components/mobile-angular-ui/demo/demo.js
+++ bower_components/mobile-angular-ui/demo/demo.js
@@ -27,14 +27,14 @@ app.config(function($routeProvider, $locationProvider) {
     $routeProvider.when('/score', {
         templateUrl: "score.html"
     });
+    $routeProvider.when('/score2', {
+        templateUrl: "score2.html"
+    });
 });
-
 app.controller('ChatController', function($rootScope, $scope, $firebase) {
     //CHAT TESTING
-    
     //CREATE A FIREBASE REFERENCE
     var ref = new Firebase("https://iisjreg-playground.firebaseio.com/chat");
-    
     // GET MESSAGES AS AN ARRAY
     $scope.messages = $firebase(ref).$asArray();
     $scope.predicate = '-ISOtime';
@@ -60,34 +60,75 @@ app.controller('ChatController', function($rootScope, $scope, $firebase) {
         }
     }
 });
-
 app.controller('ScoreController', function($rootScope, $scope, $firebase) {
-    //CHAT TESTING
-    
+    //SCORE TESTING
     //CREATE A FIREBASE REFERENCE
     var ref = new Firebase("https://iisjreg-playground.firebaseio.com/scores");
-    
-    // GET MESSAGES AS AN ARRAY
+    // GET SCORES AS AN OBJECT
     var scoresSync = $firebase(ref);
     var scores = scoresSync.$asObject();
     scores.$bindTo($scope, "scores");
-
     $scope.updateScores = function() {
-            //ALLOW CUSTOM OR ANONYMOUS USER NAMES
-            var p1Score = $scope.scores.p1Score || 0;
-            var p2Score = $scope.scores.p2Score || 0;
-
-            var time = new Date();
-            //ADD TO FIREBASE
-            console.log(p1Score);
-            console.log(p2Score);
-            scores.p1Score = p1Score;
-            scores.p2Score = p2Score;
-            scores.$save();
-
+        var p1Score = $scope.scores.p1Score || 0;
+        var p2Score = $scope.scores.p2Score || 0;
+        var time = new Date();
+        //ADD TO FIREBASE
+        console.log(p1Score);
+        console.log(p2Score);
+        scores.p1Score = p1Score;
+        scores.p2Score = p2Score;
+        scores.$save();
     }
 });
-
+app.controller('ScoreController2', function($rootScope, $scope, $firebase) {
+    //SCORE TESTING - n Players
+    //CREATE A FIREBASE REFERENCE
+    var ref = new Firebase("https://iisjreg-playground.firebaseio.com/scores2");
+    var scores = $firebase(ref).$asArray();
+    scores.$loaded().then(function() {
+        console.log(scores.length + " players");
+        var numberOfPlayers = scores.length;
+        ref.on("child_removed", function(snapshot) {
+            var deletedPost = snapshot.val();
+            console.log("Player '" + deletedPost.playerID + "' has been deleted");
+            numberOfPlayers -= 1;
+        });
+        $scope.increasePlayers = function() {
+            numberOfPlayers += 1;
+            $scope.scores.$add({
+                playerID: numberOfPlayers,
+                playerScore: 0
+            });
+        }
+        $scope.updateScore = function(score, update) {
+            console.log("Player " + score.playerID + ", +/-" + update);
+            score.playerScore += update;
+            //playerScore = playerScore + update;
+            scores.$save(score).then(function() {
+                // data has been saved to Firebase
+                console.log(" -> updated");
+            });
+        }
+        $scope.resetGame = function() {
+            for(var x in scores) {
+                if(scores[x].playerID) {
+                    console.log(x);
+                    scores[x].playerScore = 0;
+                    console.log("player " + scores[x].playerID + " score to 0");
+                    scores.$save(scores[x]).then(function() {
+                        // data has been saved to Firebase
+                        console.log(" -> updated scores");
+                    });
+                }
+            }
+        }
+        $scope.logInfo = function() {
+            console.log(scores);
+        }
+        $scope.numberOfPlayers = numberOfPlayers;
+        $scope.scores = scores;
+    });
+});
 app.controller('MainController', function($rootScope, $scope) {
     //ROUTING
     $rootScope.$on("$routeChangeStart", function() {
@@ -96,10 +137,7 @@ app.controller('MainController', function($rootScope, $scope) {
     $rootScope.$on("$routeChangeSuccess", function() {
         $rootScope.loading = false;
     });
-    
-    
-    
-    //OTHER DEMO STUFF
+    //OTHER DEMO STUFF THAT CAN EVELUATUALLY GO
     var scrollItems = [];
     for(var i = 1; i <= 100; i++) {
         scrollItems.push("Item " + i);
